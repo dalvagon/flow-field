10aa2c153ca016c2ca6f5691dc6c572b78d028e2
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2023-02-01 13:15:45 +0200

    Add new gif

--- particle.js
+++ particle.js
@@ -3,25 +3,28 @@ class Particle {
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
     this.noiseValue = 0;
-    this.speed = 2;
+    this.speed = 5;
+    this.prev_pos = this.position.copy();
   }
 
   update() {
     if (this.isOnScreen()) {
       this.noiseValue = noise(this.position.x * 0.001, this.position.y * 0.001);
-      this.velocity = p5.Vector.fromAngle(this.noiseValue * TWO_PI * 8);
+      this.velocity = p5.Vector.fromAngle(this.noiseValue * TWO_PI * 16);
       this.velocity.setMag(this.speed);
+      this.prev_pos = this.position.copy();
       this.position.add(this.velocity);
     } else {
       this.position.x = random(width);
       this.position.y = random(height);
+      this.prev_pos = this.position.copy();
     }
   }
 
   display() {
     stroke(this.noiseValue * 255, (1 - this.noiseValue) * 255, 255);
     strokeWeight(1);
-    point(this.position.x, this.position.y);
+    line(this.position.x, this.position.y, this.prev_pos.x, this.prev_pos.y);
   }
 
   isOnScreen() {
